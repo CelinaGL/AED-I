033c0882bb886913a56a314476f59798f8207b87
CelinaGL <celinagoncalveslemos@gmail.com>
2025-05-01 13:51:44 -0300

    Exercício proposto em aula

--- exercicios_aula/ex1.c
+++ exercicios_aula/ex1.c
@@ -2,6 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 
+int menu( void );
+
 int main(){
-    
+    int ent;
+
+    for(;;){
+        ent = menu();
+        switch (ent)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+            exit(0);
+            break;
+   }
+}
+
+   return 0;
+}
+
+int menu( void )
+{
+    int c; 
+    do{
+        pritnf( "1. Adicionar Nome" );
+        pritnf( "2. Remover Nome" );
+        pritnf( "3. Listar" );
+        pritnf( "4. Sair" );
+
+        scanf( "%d",&c );
+    }while(c<=0||c>4);
+
+    return c;
 }
